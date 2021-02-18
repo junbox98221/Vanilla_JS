46e5898b42bc3321c540993b654ca1b39177cb21
junbox98221 <junbox98221@gmail.com>
2021-02-19 01:23:08 +0900

    challenge_Day_8  done

--- challenge/js/day6.js
+++ challenge/js/day6.js
@@ -1,35 +1,19 @@
 const select = document.querySelector("select");
 
-function saving(country_name) {
-  localStorage.setItem("country", country_name);
-}
 // select에서 value를 가져오는 방식에 대해 더 알아야 함
 function getName() {
   const value = select.value;
-  saving(value);
+  localStorage.setItem("country", value);
 }
 
-function save_selected() {
-  select.addEventListener("input", getName);
-}
-
-function show_last(country_name) {
-  const show = document.querySelector("option[value = start]");
-  show.innerText = country_name;
-}
 // 난 value=start에 해당 국가를 입력했는데 쌤은 어떻게 했는지, 새로고침 이벤트는 따로 받아서 한건지 확인
 function get_last_value() {
   const last_code = localStorage.getItem("country");
-  if (last_code !== null) {
-    const country_name = document.querySelector(`option[value = ${last_code}]`)
-      .innerText;
-    show_last(country_name);
+  if (last_code) {
+    const country_name = document.querySelector(`option[value = ${last_code}]`);
+    country_name.selected = true;
   }
 }
 
-function init() {
-  get_last_value();
-  save_selected();
-}
-
-init();
+get_last_value();
+select.addEventListener("change", getName);
---
+++ challenge/js/day8.js
@@ -0,0 +1,151 @@
+const form = document.getElementById("js-form"),
+  input = form.querySelector("input"),
+  pendingUl = document.getElementById("js-pending"),
+  finishedUl = document.getElementById("js-finished");
+
+let pendingTasks = [],
+  finishedTasks = [];
+
+const PENDING = "PENDING";
+const FINISHED = "FINISHED";
+
+function getObj(text) {
+  return { id: Date.now(), text };
+}
+// LS에 저장하는 함수
+function saveToLS() {
+  localStorage.setItem("pending", JSON.stringify(pendingTasks));
+  localStorage.setItem("finished", JSON.stringify(finishedTasks));
+}
+// LS에 저장될 때 사용되는 전역변수를 update하는 함수
+function putPendingTasks(taskObj) {
+  pendingTasks.push(taskObj);
+  console.log(pendingTasks);
+}
+
+function putFinishedTasks(taskObj) {
+  finishedTasks.push(taskObj);
+}
+
+function deleteTask(e) {
+  const li = e.target.parentNode;
+  li.parentNode.removeChild(li);
+}
+
+function sameLiPart(taskObj) {
+  const li = document.createElement("li");
+  li.innerText = taskObj.text;
+  deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "❌";
+  li.append(deleteBtn);
+  deleteBtn.addEventListener("click", deleteTask);
+  return li;
+}
+
+function findInPending(id) {
+  return pendingTasks.find(function (obj) {
+    return obj.id == id;
+  });
+}
+function findInFinished(id) {
+  return finishedTasks.find(function (obj) {
+    return obj.id == id;
+  });
+}
+
+function removeInPending(id) {
+  pendingTasks = pendingTasks.filter(function (obj) {
+    return obj.id != id;
+  });
+}
+function removeInFinished(id) {
+  finishedTasks = finishedTasks.filter(function (obj) {
+    return obj.id != id;
+  });
+}
+
+function moveToFin(e) {
+  const li = e.target.parentNode;
+  const id = li.id;
+  const obj = findInPending(id);
+  removeInPending(id);
+  addfinishedHtml(obj);
+  deleteTask(e);
+  saveToLS();
+}
+
+function moveToPen(e) {
+  const li = e.target.parentNode;
+  const id = li.id;
+  const obj = findInFinished(id);
+  removeInFinished(id);
+  addPendingHtml(obj);
+  deleteTask(e);
+  saveToLS();
+}
+
+function addfinishedHtml(taskObj) {
+  li = sameLiPart(taskObj);
+  li.id = taskObj.id;
+  const checkBtn = document.createElement("button");
+  checkBtn.innerText = "⏪";
+  // 여기서 전역변수에 해당 taskObj를 넣기 2줄 아래에 moveToFin에서 사용되서
+  putFinishedTasks(taskObj);
+  checkBtn.addEventListener("click", moveToPen);
+  li.append(checkBtn);
+  finishedUl.append(li);
+}
+
+function addPendingHtml(taskObj) {
+  li = sameLiPart(taskObj);
+  li.id = taskObj.id;
+  const checkBtn = document.createElement("button");
+  checkBtn.innerText = "✅";
+  // 여기서 전역변수에 해당 taskObj를 넣기 2줄 아래에 moveToFin에서 사용되서
+  putPendingTasks(taskObj);
+  checkBtn.addEventListener("click", moveToFin);
+  li.append(checkBtn);
+  pendingUl.append(li);
+}
+
+//
+function handleFormSubmit(e) {
+  e.preventDefault();
+  taskObj = getObj(input.value);
+  input.value = "";
+  addPendingHtml(taskObj);
+
+  saveToLS();
+}
+
+function pendingJsonErrorHandler(pendingLS) {
+  return JSON.parse(pendingLS) || [];
+}
+
+function finishedJsonErrorHandler(finishedLS) {
+  console.log();
+  return JSON.parse(finishedLS) || [];
+}
+
+function loadPending() {
+  pending_from_LS = pendingJsonErrorHandler(localStorage.getItem("pending"));
+  pending_from_LS.forEach(function (element) {
+    addPendingHtml(element);
+  });
+}
+
+function loadFinished() {
+  finished_from_LS = finishedJsonErrorHandler(localStorage.getItem("finished"));
+  finished_from_LS.forEach(function (element) {
+    addfinishedHtml(element);
+    console.log(element);
+  });
+}
+
+function init() {
+  form.addEventListener("submit", handleFormSubmit);
+  loadPending();
+  loadFinished();
+}
+
+init();
